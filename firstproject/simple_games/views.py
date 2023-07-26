cb69845d53c6d4966cf2661146ef36ca5b91106e
Andrei Beliaev <abbot190414@gmail.com>
2023-07-26 15:04:06 +0300

    homework01: a couple of views added; some html added to earlier views;

--- firstproject/simple_games/views.py
+++ firstproject/simple_games/views.py
@@ -6,24 +6,83 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+GAME_HTML = """<html>
+        <head><title>{title}</title></head>
+        <body>
+        <div><h2>{title}</h2</div>
+            <div>
+            {result}<br><br><br>
+            {links}
+            </div>
+            <footer>
+            <a href="/">Main</a>&nbsp;&nbsp;&nbsp;
+            <a href="/about">About</a>
+            </footer>
+        </body>
+        </html>
+        """
+
+
+def index(request):
+    html = """<html>
+    <head><title>Index page</title></head>
+    <body>
+    <div><h2>Games Index Page</h2</div>
+        <div>
+            <ul>Simple Games
+                <li><a href="coinplay">Coin play</a></li>
+                <li><a href="diceplay">Dice play</a></li>
+                <li><a href="randomizer">Random number</a></li>
+            </ul>
+        </div>
+        <footer>
+        <a href="/">Main</a>&nbsp;&nbsp;&nbsp;
+        <a href="/about">About</a>
+        </footer>
+    </body>
+    </html>
+    """
+    return HttpResponse(html)
+
+
 def coin(request):
     try:
         side = ('Obverse', 'Reverse')[randint(0, 3)]
+        out = GAME_HTML.format(
+            title="Coin Play",
+            result=f"Coin side: {side}",
+            links='<a href="">Again</a><br>'
+                  '<a href="/games">Back</a><br>'
+        )
     except Exception as exc:
         logger.exception(f'Error occurred: {exc}')
-        return HttpResponse(f"Coin has stuck in air")
+        return HttpResponse(f'Coin has stuck in air<br><br><a href="">Again</a><br><a href="/games">Back</a>')
     else:
         logger.info(f'coinplay requested; coin side: {side}')
-        return HttpResponse(f"Coin side: {side}")
+        return HttpResponse(out)
 
 
 def dice(request):
     cube_side = randint(1, 6)
+    out = GAME_HTML.format(
+        title="Dice Play",
+        result=f"Cube Side Value: {cube_side}",
+        links='<a href="">Again</a><br>'
+              '<a href="/games">Back</a><br>'
+
+    )
+
     logger.info(f'diceplay requested; cube-side: {cube_side}')
-    return HttpResponse(f"Cube side: {cube_side}")
+    return HttpResponse(out)
 
 
 def random_number(request):
     answer = randint(0, 100)
     logger.info(f'random_number requested; answer: {answer}')
-    return HttpResponse(f"Random number: {answer}")
+    out = GAME_HTML.format(
+        title="Random Number",
+        result=f"Your number: {answer}",
+        links='<a href="">Again</a><br>'
+              '<a href="/games">Back</a><br>'
+    )
+    return HttpResponse(out)
