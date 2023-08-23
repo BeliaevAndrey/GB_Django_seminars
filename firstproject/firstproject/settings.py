0c1f58811da04665fa2bd7660206803f8f7c5cce
Andrei Beliaev <abbot190414@gmail.com>
2023-08-23 16:17:56 +0300

    homework 06: test-deploy prepare

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-# import os
+import os
 
 from pathlib import Path
 
@@ -21,14 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@h=)v)6*j273c@h&wz$0zm5*sbj##9)z=htzkrg^qr&tb24paj'
-# SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-@h=)v)6*j273c@h&wz$0zm5*sbj##9)z=htzkrg^qr&tb24paj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    'andrew107021.pythonanywhere.com',
 ]
 
 INTERNAL_IPS = [
@@ -84,10 +85,24 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {     # pre-deploy settings
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'andrew107021$default',
+        'USER': 'andrew107021',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'andrew107021.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init-command': "SET NAMES 'utf8mb4; SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -128,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
